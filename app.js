669e9423f08881b15328050c0c6cf2e6cb95ecb8
MDC <markdcross@gmail.com>
2020-10-21 17:22:38 -0400

    Create manager logic complete

--- app.js
+++ app.js
@@ -1,18 +1,84 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-const render = require("./lib/htmlRenderer");
+const render = require('./lib/htmlRenderer');
 
+const employeeArr = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+const createManager = () => {
+    console.log("Welcome! Let's build your team.");
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                message: "What is the manager's name?",
+                validate: (answer) => {
+                    if (answer !== '') {
+                        return true;
+                    }
+                    return 'Please provide a name.';
+                },
+            },
+            {
+                type: 'input',
+                name: 'managerID',
+                message: "What is the manager's ID number?",
+                validate: (answer) => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                        return true;
+                    }
+                    return 'Please provide a valid ID number (greater than zero).';
+                },
+            },
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: "What is the manager's email address?",
+                validate: (answer) => {
+                    const pass = answer.match(/\S+@\S+\.\S+/);
+                    if (pass) {
+                        return true;
+                    }
+                    return 'Please enter a valid email address.';
+                },
+            },
+            {
+                type: 'input',
+                name: 'managerOffice',
+                message: "What is the manager's office number?",
+                validate: (answer) => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                        return true;
+                    }
+                    return 'Please provide a valid office number (greater than zero).';
+                },
+            },
+        ])
+        .then((answers) => {
+            const manager = new Manager(
+                answers.managerName,
+                answers.managerID,
+                answers.managerEmail,
+                answers.managerOffice
+            );
+            employeeArr.push(manager);
+        });
+};
+
+createManager();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
